3d8c64d95d6a0785724d6b5dd9e14adbca813d7f
sushanthraiurwa <sushanthraikpt@gmail.com>
2025-12-03 00:14:42 +0530

    Fix: again filed soo added the lighwieght text embedding pyhton 3.13

--- rag/embedder.py
+++ rag/embedder.py
@@ -3,9 +3,7 @@ from fastembed import TextEmbedding
 embedder = TextEmbedding()
 
 def embed_text(text: str):
-    # embed returns generator → convert to list → take first
     return list(embedder.embed([text]))[0]
 
 def embed_list(texts: list):
-    # convert generator to list
     return list(embedder.embed(texts))
