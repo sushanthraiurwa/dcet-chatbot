dd8f519fd80779a257a675be72baba087ccc2b94
sushanthraiurwa <sushanthraikpt@gmail.com>
2025-12-02 21:08:14 +0530

    Fix: fixed the request time out on render

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 from rag.retriever import search
 from rag.generator import generate_answer
 
+# --------------------------
+# CREATE APP + ENABLE CORS
+# --------------------------
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            # Allow All Frontends
+    allow_credentials=True,
+    allow_methods=["*"],            # ENABLE OPTIONS METHOD
+    allow_headers=["*"],
+)
+
+# --------------------------
+# REQUEST BODY MODEL
+# --------------------------
 class Question(BaseModel):
     question: str
 
+# --------------------------
+# /ask ROUTE
+# --------------------------
 @app.post("/ask")
 def ask_question(question: Optional[str] = Query(None), body: Optional[Question] = None):
-    # Priority: body > query
+
+    # Priority for body JSON
     if body and body.question:
         question_text = body.question
     else:
@@ -21,9 +40,11 @@ def ask_question(question: Optional[str] = Query(None), body: Optional[Question]
     if not question_text:
         return {"error": "No question provided"}
 
+    # Vector search
     results = search(question_text, k=3)
     docs = results["documents"][0] if results.get("documents") else []
 
+    # LLM answer
     answer = generate_answer(docs, question_text)
 
     return {"answer": answer}
